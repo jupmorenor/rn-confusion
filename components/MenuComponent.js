b91d276fcfe5fa23f7ada56d3a81338c29a1b41a
jupmorenor <jupmorenor@gmail.com>
2023-10-13 16:45:08 -0500

    Reboot de la app con las versiones mas recientes para evitar conflictos

--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -1,19 +1,19 @@
 import React, { Component } from "react";
 import { View, FlatList } from "react-native";
-import { ListItem } from "react-native-elements";
+import { Avatar, ListItem } from "react-native-elements";
 
 const Menu = (props) => {
 
     const renderMenuItem = ({item, index}) => {
         return (
-            <ListItem 
-                key={index}
-                title={item.name}
-                subtitle={item.description}
-                hideChevron={true}
-                leftAvatar={{ source: require('./images/uthappizza.png')}}
-            />
-        )
+            <ListItem key={index} bottomDivider>
+                <Avatar source={{ uri: item.image}}/>
+                <ListItem.Content>
+                    <ListItem.Title>{item.name}</ListItem.Title>
+                    <ListItem.Subtitle>{item.description}</ListItem.Subtitle>
+                </ListItem.Content>
+            </ListItem>            
+        );
     }
 
     return (
