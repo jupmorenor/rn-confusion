a86bdbf616e201de77c31fab77a6901f8212d328
jupmorenor <jupmorenor@gmail.com>
2023-10-18 20:47:58 -0500

    Se agrega menu de navegación lateral

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,35 +1,80 @@
 import React, { Component } from "react";
-import { View, Platform } from "react-native";
+import { View } from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import Menu from './MenuComponent';
 import DishDetail from "./DishDetailComponent";
+import Home from "./HomeComponent";
 
-const MenuNavigation = createNativeStackNavigator();
+const MenuStackNavigator = createNativeStackNavigator();
+const HomeStackNavigator = createNativeStackNavigator();
+const MainDrawerNavigator = createDrawerNavigator();
+
+const RenderHome = () => {
+    return (
+        <HomeStackNavigator.Navigator initialRouteName="Home"
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: '#512DA8'
+                },
+                headerTintColor: '#fff',
+                headerTitleStyle: {
+                    color: '#fff'
+                }
+            }}
+        >
+            <HomeStackNavigator.Screen name="Home" component={Home} />
+        </HomeStackNavigator.Navigator>
+    );
+}
+
+const RenderMenu = () => {
+    return (
+        <MenuStackNavigator.Navigator initialRouteName="Menu"
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: '#512DA8'
+                },
+                headerTintColor: '#fff',
+                headerTitleStyle: {
+                    color: '#fff'
+                }
+            }}
+        >
+            <MenuStackNavigator.Screen name="Menu" component={Menu} />
+            <MenuStackNavigator.Screen name="Dishdetail" component={DishDetail} />
+        </MenuStackNavigator.Navigator>
+    );
+}
 
 class Main extends Component {
     constructor(props) {
         super(props);
     }
 
+    RenderMain() {
+        return (
+            <MainDrawerNavigator.Navigator initialRouteName="Home" screenOptions={{drawerActiveBackgroundColor: "#D1C4E9"}}>
+                <MainDrawerNavigator.Screen
+                    name="Home"
+                    options={{drawerLabel: "Home", title: "Home"}}
+                    component={RenderHome}
+                />
+                <MainDrawerNavigator.Screen 
+                    name="Menu" 
+                    options={{drawerLabel: "Menu", title: "Menu"}}
+                    component={RenderMenu}
+                />
+            </MainDrawerNavigator.Navigator>
+        );
+    }
+
     render() {
         return (
             <View style={{ flex: 1 }}>
                 <NavigationContainer>
-                    <MenuNavigation.Navigator initialRouteName="Menu"
-                        screenOptions={{
-                            headerStyle: {
-                                backgroundColor: '#512DA8'
-                            },
-                            headerTintColor: '#fff',
-                            headerTitleStyle: {
-                                color: '#fff'
-                            }
-                        }}
-                    >
-                        <MenuNavigation.Screen name="Menu" component={Menu} />
-                        <MenuNavigation.Screen name="Dishdetail" component={DishDetail} />
-                    </MenuNavigation.Navigator>
+                    <this.RenderMain />
                 </NavigationContainer>
             </View>
         );
