83ef646e8d1509570f70235c77c3f2c924e461aa
jupmorenor <jupmorenor@gmail.com>
2023-10-20 15:13:26 -0500

    Soporte para Redux tal cual como se hizo en jupmorenor/reactconfusion@6d3fd4f y posteriores

---
+++ redux/ActionCreators.js
@@ -0,0 +1,219 @@
+import * as ActionTypes from './ActionTypes';
+import { baseUrl } from '../shared/baseUrl';
+import fetch from 'cross-fetch';
+
+export const addComment = (comment) => ({
+    type: ActionTypes.ADD_COMMENT,
+    payload: comment
+});
+
+export const postComment = (dishId, rating, author, comment) => (dispatch) => {
+    const newComment = {
+        dishId: dishId,
+        rating: rating,
+        author: author,
+        comment: comment,
+    }
+    newComment.date = new Date().toISOString();
+
+    return fetch(baseUrl + 'comments', {
+        method: 'POST',
+        body: JSON.stringify(newComment),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        credentials: 'same-origin',
+    }).then(response => {
+        if (response.ok) {
+            return response;
+        } else {
+            var error = new Error('Error ' + response.status + ': ' + response.statusText);
+            error.response = response;
+            throw error;
+        }
+    }, error => {
+        var errmess = new Error(error.message);
+        throw errmess;
+    }).then(response => response.json())
+    .then(comment => dispatch(addComment(comment)))
+    .catch(error => {
+        console.log('Post comments', error.message);
+        alert('No se ha podido publicar el comentario.');
+    });
+        
+    //.catch(error => dispatch(commentsFailed(error.message)));
+}
+
+export const addFeedback = (feedback) => ({
+    type: ActionTypes.ADD_FEEDBACK,
+    payload: feedback
+});
+
+export const postFeedback = (feedback) => (dispatch) => {
+
+    // feedback.date = new Date().toISOString();
+
+    return fetch(baseUrl + 'feedback', {
+        method: 'POST',
+        body: JSON.stringify(feedback),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        credentials: 'same-origin',
+    }).then(response => {
+        if (response.ok) {
+            return response;
+        } else {
+            var error = new Error('Error ' + response.status + ': ' + response.statusText);
+            error.response = response;
+            throw error;
+        }
+    }, error => {
+        var errmess = new Error(error.message);
+        throw errmess;
+    }).then(response => response.json())
+    .then(feedback => {
+        dispatch(addFeedback(feedback));
+        alert('Gracias por el feedback.\n' + JSON.stringify(feedback));
+    })
+    .catch(error => {
+        console.log('Post feedback', error.message);
+        alert('No se ha podido publicar el feedback.');
+    });
+}
+
+// Dishes
+export const fetchDishes = () => (dispatch) => {
+    dispatch(dishesLoading(true));
+
+    return fetch(baseUrl + 'dishes')
+        .then(response => {
+            if (response.ok) {
+                return response;
+            } else {
+                var error = new Error('Error ' + response.status + ': ' + response.statusText);
+                error.response = response;
+                throw error;
+            }
+        }, error => {
+            var errmess = new Error(error.message);
+            throw errmess;
+        }).then(response => response.json())
+        .then(dishes => dispatch(addDishes(dishes)))
+        .catch(error => dispatch(dishesFailed(error.message)));
+}
+
+export const dishesLoading = () => ({
+    type: ActionTypes.DISHES_LOADING,
+});
+
+export const dishesFailed = (err) => ({
+    type: ActionTypes.DISHES_FAILED,
+    payload: err,
+});
+
+export const addDishes = (dishes) => ({
+    type: ActionTypes.ADD_DISHES,
+    payload: dishes,
+});
+
+// Comments
+export const fetchComments = () => (dispatch) => {
+    return fetch(baseUrl + 'comments')
+        .then(response => {
+            if (response.ok) {
+                return response;
+            } else {
+                var error = new Error('Error ' + response.status + ': ' + response.statusText);
+                error.response = response;
+                throw error;
+            }
+        }, error => {
+            var errmess = new Error(error.message);
+            throw errmess;
+        })
+        .then(response => response.json())
+        .then(comments => dispatch(addComments(comments)))
+        .catch(error => dispatch(commentsFailed(error.message)));
+}
+
+export const commentsFailed = (err) => ({
+    type: ActionTypes.COMMENTS_FAILED,
+    payload: err,
+});
+
+export const addComments = (comments) => ({
+    type: ActionTypes.ADD_COMMENTS,
+    payload: comments,
+});
+
+// Promotions
+export const fetchPromotions = () => (dispatch) => {
+    dispatch(promotionsLoading(true));
+
+    return fetch(baseUrl + 'promotions')
+        .then(response => {
+            if (response.ok) {
+                return response;
+            } else {
+                var error = new Error('Error ' + response.status + ': ' + response.statusText);
+                error.response = response;
+                throw error;
+            }
+        }, error => {
+            var errmess = new Error(error.message);
+            throw errmess;
+        })
+        .then(response => response.json())
+        .then(promotions => dispatch(addPromotions(promotions)))
+        .catch(error => dispatch(promotionsFailed(error.message)));
+}
+
+export const promotionsLoading = () => ({
+    type: ActionTypes.PROMOTIONS_LOADING,
+});
+
+export const promotionsFailed = (err) => ({
+    type: ActionTypes.PROMOTIONS_FAILED,
+    payload: err,
+});
+
+export const addPromotions = (promotions) => ({
+    type: ActionTypes.ADD_PROMOTIONS,
+    payload: promotions,
+});
+
+// Leaders
+export const fetchLeaders = () => (dispatch) => {
+    dispatch(leadersLoading(true));
+
+    return fetch(baseUrl + 'leaders')
+        .then(response => {
+            if (response.ok) {
+                return response;
+            } else {
+                var error = new Error('Error ' + response.status + ': ' + response.statusText);
+                error.response = response;
+                throw error;
+            }
+        }, error => {
+            var errmess = new Error(error.message);
+            throw errmess;
+        }).then(response => response.json())
+        .then(leaders => dispatch(addLeaders(leaders)))
+        .catch(error => dispatch(leadersFailed(error.message)));
+}
+
+export const leadersLoading = () => ({
+    type: ActionTypes.LEADERS_LOADING,
+});
+
+export const leadersFailed = (err) => ({
+    type: ActionTypes.LEADERS_FAILED,
+    payload: err,
+});
+
+export const addLeaders = (leaders) => ({
+    type: ActionTypes.ADD_LEADERS,
+    payload: leaders,
+});
---
+++ redux/forms.js
@@ -0,0 +1,9 @@
+export const InitialFeedack = {
+    firstname: '',
+    lastname: '',
+    telnum: '',
+    email: '',
+    agree: false,
+    contactType: 'Tel.',
+    message: ''
+}
