edb7c6dd917e39ada536fe2580111c8d06d8c792
jupmorenor <jupmorenor@gmail.com>
2023-10-24 19:40:45 -0500

    Gestures con PanResponder

--- components/DishDetailComponent.js
+++ components/DishDetailComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text, ScrollView, FlatList, Modal, StyleSheet, Button } from "react-native";
+import { View, Text, ScrollView, FlatList, Modal, StyleSheet, Button, PanResponder, Alert } from "react-native";
 import { Card, Icon, Rating, Input } from "react-native-elements";
 import { connect } from "react-redux";
 import { baseUrl } from '../shared/baseUrl';
@@ -21,27 +21,56 @@ const mapDispatchToProps = dispatch => ({
 
 const RenderDish = (props) => {
     const dish = props.dish;
+    var view;
+    const handleViewRef = ref => view = ref;
+
+    const recognizeDrag = ({ moveX, moveY, dx, dy }) => {
+        return ( dx < -200 );
+    }
+
+    const panResponder = PanResponder.create({
+        onStartShouldSetPanResponder: (e, gestureState) => {
+            return true;
+        },
+        onPanResponderGrant: () => {view.rubberBand(1000).then(endState => console.log(endState.finished ? 'finished' : 'cancelled'));},
+        onPanResponderEnd: (e, gestureState) => {
+            if (recognizeDrag(gestureState))
+                Alert.alert(
+                    'Add Favorite',
+                    'Are you sure you wish to add ' + dish.name + ' to favorite?',
+                    [
+                        {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+                        {text: 'OK', onPress: () => {props.favorite ? console.log('Already favorite') : props.onPress()}},
+                    ],
+                    { cancelable: false }
+                );
+
+            return true;
+        }
+    })
 
     if (dish !== null) {
         return (
-            <Card>
-                <Card.Image source={{ uri: baseUrl + dish.image }}>
-                    <Card.FeaturedTitle>{dish.name}</Card.FeaturedTitle>
-                </Card.Image>
-                <Card.Divider />
-                <Text style={{margin: 10}} >
-                    {dish.description}
-                </Text>
-                <View style={styles.modalContent}>
-                    <Icon style={{flex: 1}} raised reverse type="font-awesome" color='#f50'
-                        name={ props.favorite ? 'heart': 'heart-o'}
-                        onPress={() => props.favorite ? console.log('fav') : props.onPress()}
-                    />
-                    <Icon style={{flex: 1}} raised reverse type="font-awesome" color="#512DA8" name="pencil"
-                        onPress={() => props.toggleModal()}
-                    />
-                </View>
-            </Card>
+            <Animatable.View ref={handleViewRef} animation="fadeInDown" duration={2000} delay={1000} {...panResponder.panHandlers} >
+                <Card>
+                    <Card.Image source={{ uri: baseUrl + dish.image }}>
+                        <Card.FeaturedTitle>{dish.name}</Card.FeaturedTitle>
+                    </Card.Image>
+                    <Card.Divider />
+                    <Text style={{margin: 10}} >
+                        {dish.description}
+                    </Text>
+                    <View style={styles.modalContent}>
+                        <Icon style={{flex: 1}} raised reverse type="font-awesome" color='#f50'
+                            name={ props.favorite ? 'heart': 'heart-o'}
+                            onPress={() => props.favorite ? console.log('fav') : props.onPress()}
+                        />
+                        <Icon style={{flex: 1}} raised reverse type="font-awesome" color="#512DA8" name="pencil"
+                            onPress={() => props.toggleModal()}
+                        />
+                    </View>
+                </Card>
+            </Animatable.View>
         );
     } else {
         return (
@@ -114,14 +143,12 @@ class DishDetail extends Component {
         const dishId = +this.props.route.params.dishId;
         return (
             <ScrollView>
-                <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
-                    <RenderDish
-                        dish={this.props.dishes.dishes[dishId]}
-                        favorite={this.props.favorites.some(el => el === dishId)}
-                        onPress={() => this.markFavorite(dishId)}
-                        toggleModal={() => this.toggleModal()}
-                    />
-                </Animatable.View>
+                <RenderDish
+                    dish={this.props.dishes.dishes[dishId]}
+                    favorite={this.props.favorites.some(el => el === dishId)}
+                    onPress={() => this.markFavorite(dishId)}
+                    toggleModal={() => this.toggleModal()}
+                />
                 <Animatable.View animation="fadeInUp" duration={2000} delay={1000}>
                     <RenderComments comments={this.props.comments.comments.filter((comment) => comment.dishId === dishId)} />
                 </Animatable.View>
